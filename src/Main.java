9539089c38a49000537563e7134f32afcb7f9fe2
Agustín Toranza <agtoranza@gmail.com>
2025-08-13 14:12:48 -0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // Mas adelante vamos a ver mejor este tema
+        ascensor ascensorPrimeroDeIndepencia = new ascensor();
 
+        ascensorPrimeroDeIndepencia.abrirPuerta();
+        System.out.println("Esta abierta la puerta???" + ascensorPrimeroDeIndepencia.estaAbiertaLaPuerta());
+        System.out.println("En que piso estas?? " + ascensorPrimeroDeIndepencia.enQuePisoEstas());
+        ascensorPrimeroDeIndepencia.andaAlPiso(10);
+        System.out.println("En que piso estas?? " + ascensorPrimeroDeIndepencia.enQuePisoEstas());
+        ascensorPrimeroDeIndepencia.cerrarPuerta();
+        System.out.println("Esta abierta la puerta???" + ascensorPrimeroDeIndepencia.estaAbiertaLaPuerta());
     }
 }
